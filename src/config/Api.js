2a3f1c6ab1e72bb31a8c34b10b00e5d497e19037
Faeq <faequl96@gmail.com>
2023-01-18 18:06:12 +0700

    env updated

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-   baseURL: "https://waysbucks.osc-fr1.scalingo.io/api/v1",
+   baseURL: process.env.REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
