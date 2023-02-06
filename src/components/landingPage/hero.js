1b3ae6e4333de5acc98a0bf9bd0f684522ded02d
Faeq <faequl96@gmail.com>
2023-02-06 23:48:09 +0700

    image element to mini size for performance app

--- src/components/landingPage/hero.js
+++ src/components/landingPage/hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <div className="bg-red-700 lg:bg-white mt-20 lg:mt-32 py-4">
       <div className="lg:relative mx-auto max-w-6xl 2xl:max-w-7xl lg:px-10">
-        <div className="pr-3 hidden lg:block">
-          <img src={hero} />
+        <div className="hidden lg:block">
+          <img src={hero} className="w-full" />
         </div>
         <div className="lg:absolute md:flex md:items-center md:justify-between lg:top-16 xl:top-24 lg:left-24 xl:left-24 lg:w-[40%]">
           <div className="md:w-[50%] md:ml-6 lg:w-full">
@@ -18,7 +18,7 @@ const Hero = () => {
               </h1>
             </div>
             <div className="pr-3 md:hidden">
-              <img src={heroImg} />
+              <img src={heroImg} className="w-full"/>
             </div>
             <div className="px-3">
               <h5 className="font-semibold text-red-50 mb-3 leading-6 lg:mb-4 mt-3 text-lg lg:text-lg">
