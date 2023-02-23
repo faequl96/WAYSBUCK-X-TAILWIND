5beb63bfa094b418d56719ec29a072bb646b4f9e
faequl77 <xslankers@outlook.com>
2023-02-23 16:24:44 +0700

    change the REST APIs

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-   baseURL: "https://waysbucks.osc-fr1.scalingo.io/api/v1",
+   baseURL: "https://wesbak.osc-fr1.scalingo.io/api/v1",
 });
 
 export const setAuthToken = (token) => {
